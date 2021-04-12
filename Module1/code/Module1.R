5dc20d26e53ed1327a038dad010d06b8a14cf7c2
sarithdm <68165093+sarithdm@users.noreply.github.com>
2021-04-13 00:21:04 +0530

    Update Module1.R

--- Module1/code/Module1.R
+++ Module1/code/Module1.R
@@ -3,7 +3,10 @@ library(readr)
 #Working with well-structured data from files or URLs
 #LOADING WELL-STRUCTURED DATA FROM FILES OR URLS
 #Listing 2.1 Reading the UCI car data
-ucicar <- read_csv("GitHub/cs466/Module1/data/car.data.csv")
+uciCar <- read_csv("GitHub/cs466/Module1/data/car.data.csv")
 #EXAMINING OUR DATA
 #Listing 2.2 Exploring the car data
-class()
+class(uciCar)
+summary(uciCar)
+dim(uciCar)
+
