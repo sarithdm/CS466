786a3c163c33af317d22e8a93419fce37041eb00
sarithdm <68165093+sarithdm@users.noreply.github.com>
2021-05-10 23:55:35 +0530

    Code and Data

---
+++ Module2/code/Module2.R
@@ -0,0 +1,80 @@
+#Listing 6.2 Plotting churn grouped by variable 218 levels
+table218 <- table(
+  Var218=dTrain[,'Var218'],
+  churn=dTrain[,outcome],
+  useNA='ifany')
+print(table218)
+#Listing 6.3 Churn rates grouped by variable 218 codes
+print(table218[,2]/(table218[,1]+table218[,2]))
+#Listing 6.4 Function to build single-variable models for categorical variables
+mkPredC <- function(outCol,varCol,appCol) {
+  pPos <- sum(outCol==pos)/length(outCol)
+  naTab <- table(as.factor(outCol[is.na(varCol)]))
+  pPosWna <- (naTab/sum(naTab))[pos]
+  vTab <- table(as.factor(outCol),varCol)
+  pPosWv <- (vTab[pos,]+1.0e-3*pPos)/(colSums(vTab)+1.0e-3)
+  pred <- pPosWv[appCol]
+  pred[is.na(appCol)] <- pPosWna
+  pred[is.na(pred)] <- pPos
+  pred
+}
+#Listing 6.5 Applying single-categorical variable models to all of our datasets
+for(v in catVars) {
+  pi <- paste('pred',v,sep='')
+  dTrain[,pi] <- mkPredC(dTrain[,outcome],dTrain[,v],dTrain[,v])
+  dCal[,pi] <- mkPredC(dTrain[,outcome],dTrain[,v],dCal[,v])
+  dTest[,pi] <- mkPredC(dTrain[,outcome],dTrain[,v],dTest[,v])
+}
+#Listing 6.6 Scoring categorical variables by AUC
+#library('ROCR')
+calcAUC <- function(predcol,outcol) {
+  perf <- performance(prediction(predcol,outcol==pos),'auc')
+  as.numeric(perf@y.values)
+}
+ for(v in catVars) {
+  pi <- paste('pred',v,sep='')
+  aucTrain <- calcAUC(dTrain[,pi],dTrain[,outcome])
+  if(aucTrain>=0.8) {
+    aucCal <- calcAUC(dCal[,pi],dCal[,outcome])
+    print(sprintf("%s, trainAUC: %4.3f calibrationAUC: %4.3f",
+                  pi,aucTrain,aucCal))
+  }
+ }
+#Using numeric features
+#Listing 6.7 Scoring numeric variables by AUC
+mkPredN <- function(outCol,varCol,appCol) {
+  cuts <- unique(as.numeric(quantile(varCol,
+                                     probs=seq(0, 1, 0.1),na.rm=T)))
+  varC <- cut(varCol,cuts)
+  appC <- cut(appCol,cuts)
+  mkPredC(outCol,varC,appC)
+}
+for(v in numericVars) {
+  pi <- paste('pred',v,sep='')
+  dTrain[,pi] <- mkPredN(dTrain[,outcome],dTrain[,v],dTrain[,v])
+  dTest[,pi] <- mkPredN(dTrain[,outcome],dTrain[,v],dTest[,v])
+  dCal[,pi] <- mkPredN(dTrain[,outcome],dTrain[,v],dCal[,v])
+  aucTrain <- calcAUC(dTrain[,pi],dTrain[,outcome])
+  if(aucTrain>=0.55) {
+    aucCal <- calcAUC(dCal[,pi],dCal[,outcome])
+    print(sprintf("%s, trainAUC: %4.3f calibrationAUC: %4.3f",
+                  pi,aucTrain,aucCal))
+  }
+}
+#Listing 6.8 Plotting variable performance
+library(ggplot2)
+ggplot(data=dCal) +
+  geom_density(aes(x=predVar126,color=as.factor(churn)))
+#Using cross-validation to estimate effects of overfitting
+#Listing 6.9 Running a repeated cross-validation experiment
+var <- 'Var217'
+aucs <- rep(0,100)
+for(rep in 1:length(aucs)) {
+  useForCalRep <- rbinom(n=dim(dTrainAll)[[1]],size=1,prob=0.1)>0
+  predRep <- mkPredC(dTrainAll[!useForCalRep,outcome],
+                     dTrainAll[!useForCalRep,var],
+                     dTrainAll[useForCalRep,var])
+  aucs[rep] <- calcAUC(predRep,dTrainAll[useForCalRep,outcome])
+}
+mean(aucs)
+sd(aucs)
