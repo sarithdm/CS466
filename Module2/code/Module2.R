b22d68a824d18e71a8efc39dc67293777f162b29
sarithdm <68165093+sarithdm@users.noreply.github.com>
2021-05-11 00:35:31 +0530

    Code and Data

--- Module2/code/Module2.R
+++ Module2/code/Module2.R
@@ -103,3 +103,49 @@ x <- c("PWGT",
        riskfactors)
 fmla <- paste(y, paste(x, collapse="+"), sep="~")
 print(fmla)
+#Cluster analysis
+#Listing 8.1 Reading the protein data
+protein <- read.delim("~/GitHub/cs466/Module2/data/protein.txt")
+View(protein)
+summary(protein)
+#Listing 8.2 Rescaling the dataset
+vars.to.use <- colnames(protein)[-1]
+pmatrix <- scale(protein[,vars.to.use])
+pcenter <- attr(pmatrix, "scaled:center")
+pscale <- attr(pmatrix, "scaled:scale")
+#Listing 8.3 Hierarchical clustering
+d <- dist(pmatrix, method="euclidean")
+pfit <- hclust(d, method="ward")
+plot(pfit, labels=protein$Country)
+rect.hclust(pfit, k=5)
+#Listing 8.4 Extracting the clusters found by hclust()
+groups <- cutree(pfit, k=5)
+print_clusters <- function(labels, k) {
+  for(i in 1:k) {
+    print(paste("cluster", i))
+    print(protein[labels==i,c("Country","RedMeat","Fish","Fr.Veg")])
+  }
+}
+print_clusters(groups, 5)
+
+#Listing 8.5 Projecting the clusters on the first two principal components
+library(ggplot2)
+princ <- prcomp(pmatrix)
+nComp <- 2
+project <- predict(princ, newdata=pmatrix)[,1:nComp]
+project.plus <- cbind(as.data.frame(project),
+                      cluster=as.factor(groups),
+                      country=protein$Country)
+ggplot(project.plus, aes(x=PC1, y=PC2)) +
+  geom_point(aes(shape=cluster)) +
+  geom_text(aes(label=country),
+            hjust=0, vjust=1)
+
+#library(fpc)
+#The k-means algorithm
+kbest.p<-5
+pclusters <- kmeans(pmatrix, kbest.p, nstart=100, iter.max=100)
+summary(pclusters)
+pclusters$centers
+groups <- pclusters$cluster
+print_clusters(groups, kbest.p)
